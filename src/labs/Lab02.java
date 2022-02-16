ad20b98dfc0309d905e003a321a1a9bdd8b69ee4
prematch1 <ilira@newtech.coppellisd.com>
2022-02-16 14:59:54 -0600

    Update Lab02.java

--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -16,9 +16,20 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in);
+		 System.out.print("Please enter a positive integer: ");
+		 int Number1 = inKey.nextInt();
+		 System.out.print("Please enter a positive integer: ");
+		 int Number2 = inKey.nextInt();
 		
-		
-		
+		 int scope = Number2 - Number1;
+			int min = 1;
+
+			for (int i = 0; i > 10; i++ ) {
+				int num = (int) (scope + min);
+				System.out.println(num);
+				
+			}
 		
 	}
 	
